d60d1e6efc4bd87e3edacab190c1f2146bf2ccde
Ian <iankosgey535@gmail.com>
2024-01-07 18:48:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+input.addEventListener('click', function() {
+  alert('Huraaay! I learnt to add an event listener.');
+});
 }
